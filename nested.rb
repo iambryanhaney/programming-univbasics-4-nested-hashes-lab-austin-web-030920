1524166f012200cfd2fc3f4273112f509081dc3b
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-02-29 13:32:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] => {:known_for => "Ruby", :languages => "C"}
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => "C"}
     
 end
 
